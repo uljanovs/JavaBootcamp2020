2c1ffe42bb3f2b4d39e95ebb3c7f7bac7bbb39d2
Ulyanov <alexander.ulyanov@accenture.com>
2020-02-23 14:34:26 +0200

    SEF

---
+++ SEF/src/sef/module2/sample/HelloWorld.java
@@ -0,0 +1,11 @@
+package sef.module2.sample;
+
+public class HelloWorld {
+
+    public static void main(String arg[]) {
+        System.out.println("Hello Bootcampers! " +
+                "\nWelcome to the Java course!"
+                +
+                "\nI love You!");
+    }
+}
