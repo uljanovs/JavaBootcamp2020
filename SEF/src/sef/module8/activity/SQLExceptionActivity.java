3e1635363ab17812a1bb93350ede5fad0c60c110
Ulyanov <alexander.ulyanov@accenture.com>
2020-02-27 17:41:29 +0200

    Work done during DAY 4

--- SEF/src/sef/module8/activity/SQLExceptionActivity.java
+++ SEF/src/sef/module8/activity/SQLExceptionActivity.java
@@ -1,5 +1,6 @@
 package sef.module8.activity;
 //Needs to be completed
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,25 +9,31 @@ import java.sql.SQLException;
 public class SQLExceptionActivity {
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-			String url = "jdbc:mysql://localhost/activity1"; 
-			String user = "root";
-			String pass = "abcd1234";
-			
-			//The following code would not compile unless it's put inside a try catch
-			//1 - put it in a try block and handle ClassNotFoundException
-			
-			
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection cn = DriverManager.getConnection(url, user, pass);
-			System.out.println("Connection successfully established! \n");
-			
-			cn.close();
-			
-			//2 - You also need to catch SQLException for it to compile
-			
-	
-	}
+        String url = "jdbc:mysql://localhost/activity1";
+        String user = "root";
+        String pass = "abcd1234";
+
+        //The following code would not compile unless it's put inside a try catch
+        //1 - put it in a try block and handle ClassNotFoundException
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+        } catch (ClassNotFoundException c) {
+            System.out.println("The driver class is not found");
+        }
+
+        //2 - You also need to catch SQLException for it to compile
+
+        try {
+            Connection cn = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connection successfully established! \n");
+            cn.close();
+        } catch (SQLException s) {
+            System.out.println("There is an SQLException. Please check the connection parameters.");
+        }
+    }
 
 }
